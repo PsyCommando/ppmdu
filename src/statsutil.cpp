5f32471fad8bcabf5c290a91427ff12f182405a7
psy_commando <psycommando@gmail.com>
2016-08-24 22:16:43 -0400

    Statsutil: Bugfix Release 0.23.1
    * Fixed issue #33

--- src/statsutil.cpp
+++ src/statsutil.cpp
@@ -108,7 +108,7 @@ namespace statsutil
 //------------------------------------------------
     const string CStatsUtil::Exe_Name            = "ppmd_statsutil.exe";
     const string CStatsUtil::Title               = "Game data importer/exporter";
-    const string CStatsUtil::Version             = "0.23";
+    const string CStatsUtil::Version             = "0.23.1";
     const string CStatsUtil::Short_Description   = "A utility to export and import various game statistics/data, such as pokemon stats.";
     const string CStatsUtil::Long_Description    = 
         "To export game data to XML, you have to append \"-e\" to the\ncommandline, followed with the option corresponding to what to export.\n"
